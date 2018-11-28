82b9882e759fe2068987f61d2767c36e10a704f8
Alfredo Rodriguez <33364219+AlfredoRdgz@users.noreply.github.com>
2018-11-28 13:52:26 -0600

    Update Start.java

--- src/application/Start.java
+++ src/application/Start.java
@@ -1,18 +1,31 @@
 package application;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Start extends Application {
 
+	public static ResourceBundle rb;
 	@Override
 	public void start(Stage stage) throws Exception {
+		
+		HBox navbar=new HBox();
+		ComboBox<String> language= new ComboBox<String>();
+		language.getItems().addAll("Español","English");
+		language.setValue(rb.getString("select_cbox"));
+		navbar.getChildren().add(language);
+		
 		VBox main = new VBox();
-		Button complexNumBtn = new Button("Números Complejos");
+		Button complexNumBtn = new Button(rb.getString("complex_title"));
 		complexNumBtn.setPrefWidth(280d);
 		complexNumBtn.setPrefHeight(20d);
 		complexNumBtn.setOnAction((event) -> {
@@ -20,7 +33,6 @@ public class Start extends Application {
 			try {
 				complexNumGUI.start(stage);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
@@ -32,7 +44,6 @@ public class Start extends Application {
 			try {
 				vectorsGUI.start(stage);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
@@ -40,11 +51,10 @@ public class Start extends Application {
 		matrixBtn.setPrefWidth(280d);
 		matrixBtn.setPrefHeight(20d);
 		matrixBtn.setOnAction((event) -> {
-			MatrixGUI matrixGUI = new MatrixGUI();
+			MatrixGUI matrixGUI = new MatrixGUI(rb);
 			try {
 				matrixGUI.start(stage);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
@@ -56,7 +66,6 @@ public class Start extends Application {
 			try {
 				linearTransformGUI.start(stage);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
@@ -64,17 +73,35 @@ public class Start extends Application {
 		chatBtn.setPrefWidth(280d);
 		chatBtn.setPrefHeight(20d);
 		chatBtn.setOnAction((event) -> {
-			ChatClient chat=new ChatClient();
+			ChatClient chat=new ChatClient(rb);
 			try {
 				chat.start(stage);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		});
+		
+		language.setOnAction((event) -> {
+			String resourcesPath = "resources.Bundle";
+			switch(language.getValue().toLowerCase()) {
+				case "español":
+					rb = ResourceBundle.getBundle(resourcesPath, new Locale("es", "mx"));
+					System.out.println("Idioma modificado");
+					System.out.println(rb.getLocale());					
+					break;
+				case "english":
+					rb = ResourceBundle.getBundle(resourcesPath, new Locale("en", "us"));
+					System.out.println("Language changed");
+					System.out.println(rb.getLocale());
+					break;
+				default:
+					break;
+			}
+		});
 
 		main.setSpacing(5d);
 		main.setPadding(new Insets(10, 0, 0, 10));
-		main.getChildren().addAll(complexNumBtn, vectorsBtn, matrixBtn, ltBtn,chatBtn);
+		main.getChildren().addAll(navbar,complexNumBtn, vectorsBtn, matrixBtn, ltBtn,chatBtn);
 
 		Scene scene = new Scene(main, 300, 150);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -85,10 +112,16 @@ public class Start extends Application {
 		stage.centerOnScreen();
 		stage.setTitle("Linear FX");
 		stage.show();
+		
+		
 	}
-
+	
+	
 	public static void main(String[] args) {
+		Locale locale = new Locale("es", "mx");
+		String resourcesPath = "resources.Bundle";
+		rb = ResourceBundle.getBundle(resourcesPath,locale);
 		launch(args);
 	}
-
+	
 }
